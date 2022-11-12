56d1c993650c3a9eaf3260cf29a224a4464e3c07
Nahuel Giannantonio <nahuelgiannantonio@gmail.com>
2022-11-12 20:12:15 +0100

    "versionFinal"

--- Front End - Parte2/6 DOM JS/Manipulando el DOM/script.js
+++ Front End - Parte2/6 DOM JS/Manipulando el DOM/script.js
@@ -1,6 +1,7 @@
-( () => { //Esto es para que el usuario no tenga acceso a las function's
-const btn = document.querySelector('[data-form-btn]');
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
+const btn = document.querySelector('[data-form-btn]');
 //Cree el const junto con la arrow function
 const createTask = (evento) =>{
   evento.preventDefault();
@@ -17,27 +18,11 @@ const createTask = (evento) =>{
   titleTask.innerText = value;  
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
-  const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
   //task.innerHTML = content;
   task.appendChild(taskContent);
   list.appendChild(task); //Agregando un hijo al elemto list, es decir 'task' al 'list' 
-
+  task.appendChild(deleteIcon());
 }
 //Evento que llama a la arrow function
 btn.addEventListener('click', createTask)
 
-const checkComplete = () => {
-  const i = document.createElement('i');
-  i.classList.add('far','fa-check-square','icon')
-  i.addEventListener('click', completeTask)
-  return i
-}
-// Immediately invoked function expression IIFE
-const completeTask = (event) =>{
-  const element =event.target;
-  element.classList.toggle('fas');
-  element.classList.toggle('completeIcon');
-  element.classList.toggle('far');
-}
-})()
